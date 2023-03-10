37762a50f04b595fdfdb545752e3a48f08f1b1c6
em-ess <michssong@gmail.com>
2023-03-09 18:24:11 -0600

    Initialized Task app. Still error with adding taskInput.

---
+++ src/components/dashboard/TaskList.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let tasks;
+</script>
+
+<div class='rounded-lg shadow-lg p-8 flex flex-col gap-4'>
+    {#each tasks as item,index}
+    <div class='rounded-md p-4 justify-between flex flex-row border-gray-800 border-2'>
+        <div class='font-medium text-sm'>{item.text}</div>
+        <button class>x</button>
+    </div>
+    {/each}
+</div>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import TaskForm from "../../components/dashboard/TaskForm.svelte";
+    import TaskList from "../../components/dashboard/TaskList.svelte";
+
+    //state
+
+    let taskInput = '';
+
+    let taskList = [{
+        text: "Buy sour straws",
+        status: true
+    }, {
+        text: "Make spam musubi",
+        status: false
+    }];
+
+    function addToList() {
+        taskList = [
+           { text: taskInput, status: true}, ...taskList
+        ]
+    }
+</script>>
+
+<div class="h-screen">
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-16">
+            <TaskForm taskInput={taskInput} add={addToList}/>
+            <TaskList tasks={taskList} />
+        </div>
+    </div>
+</div>
